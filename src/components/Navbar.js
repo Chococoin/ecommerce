8ebe5b0cf8ee704321f0e377ed6750b326b5136e
Germán Lugo <german.lugo@bitnibs.com>
2019-02-14 11:18:44 +0100

    Style Navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
 import ButtonContainer from './Button';
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link{
+    color: var(--mainWhite) !important;
+    font: 1.3rem;
+    text-transform: capitalize;
+  }
+`
 
 class Navbar extends React.Component {
   render(){
     return(
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to='/'>
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -25,7 +35,7 @@ class Navbar extends React.Component {
           </span>
          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     )
   }
 }
